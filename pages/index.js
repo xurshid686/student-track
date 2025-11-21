713c0a93318669ff0f6ce411374c0d0f104c7fa0
xurshid686 <aliqulovxurshid24@gmail.com>
2025-11-21 23:58:52 +0500

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+export default function Home() {
+  const router = useRouter()
+
+  useEffect(() => {
+    router.push('/login')
+  }, [router])
+
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="text-center">
+        <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-primary-500 mx-auto"></div>
+        <p className="mt-4 text-gray-600">Redirecting to login...</p>
+      </div>
+    </div>
+  )
+}
